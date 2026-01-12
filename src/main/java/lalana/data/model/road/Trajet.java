e3462f0da49840ef47475799a8d3e48538bc613f
tonymushah <tonyramanantsalama@gmail.com>
2026-01-12 22:48:15 +0300

    some model fixes

--- src/main/java/lalana/data/model/road/Trajet.java
+++
@@ -1,9 +0,0 @@
-package lalana.data.model.road;
-
-import lalana.data.model.car.Voiture;
-
-public class Trajet {
-    Voiture v;
-    double vitesseMoyenne;
-    Lalana lalana;
-}
---
+++ src/main/java/lalana/model/Trajet.java
@@ -0,0 +1,10 @@
+package lalana.model;
+
+import lalana.data.model.car.Voiture;
+import lalana.data.model.road.Lalana;
+
+public class Trajet {
+	Voiture v;
+	double vitesseMoyenne;
+	Lalana lalana;
+}
