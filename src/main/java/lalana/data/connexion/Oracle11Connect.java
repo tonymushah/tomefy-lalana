4b1f802d8539680fd32c775d62cc925ebe4c56ed
tonymushah <tonyramanantsalama@gmail.com>
2026-01-12 12:49:55 +0300

    connect

--- src/main/java/lalana/data/connexion/Oracle11Connect.java
+++ src/main/java/lalana/data/connexion/Oracle11Connect.java
@@ -1,5 +1,19 @@
 package lalana.data.connexion;
 
-public class Oracle11Connect {
-        //CONNEXTION  sur l'info de la route
+import io.github.cdimascio.dotenv.Dotenv;
+import mg.tonymushah.dbconnection.Oracle11gConnect;
+
+public class Oracle11Connect extends Oracle11gConnect {
+
+    public Oracle11Connect(String host, int port, String dbName,
+            String username, String password) {
+        super("jdbc", "oracle", "thin", host, port, dbName, username, password);
+    }
+
+    public Oracle11Connect(Dotenv dotenv) {
+        this(dotenv.get("ORACLE_HOST"), Integer.parseInt(dotenv.get("ORACLE_PORT")),
+                dotenv.get("ORACLE_DB_NAME"), dotenv.get("ORACLE_DB_USER"),
+                dotenv.get("ORACLE_DB_PASSWORD"));
+    }
+    // CONNEXTION sur l'info de la route
 }
--- src/main/java/lalana/data/connexion/PostgresConnect.java
+++ src/main/java/lalana/data/connexion/PostgresConnect.java
@@ -1,6 +1,19 @@
 package lalana.data.connexion;
 
-public class PostgresConnect {
-        //CONNEXTION  sur l'info de la voiture
+import io.github.cdimascio.dotenv.Dotenv;
+import mg.tonymushah.dbconnection.PostGresConnect;
+
+public class PostgresConnect extends PostGresConnect {
+
+	public PostgresConnect(String host, int port, String dbName, String username, String password) {
+		super(host, port, dbName, username, password);
+		// TODO Auto-generated constructor stub
+	}
+
+	public PostgresConnect(Dotenv dotenv) {
+		this(dotenv.get("POSTGRES_HOST"), Integer.parseInt(dotenv.get("POSTGRES_PORT")), dotenv.get("POSTGRES_DB_NAME"),
+				dotenv.get("POSTGRES_USERNAME"), dotenv.get("POSTGRES_PASSWORD"));
+	}
+	// CONNEXTION sur l'info de la voiture
 
 }
