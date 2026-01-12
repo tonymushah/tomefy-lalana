8a2e2d8621ee8a51d762d04b391e9900fee98462
tonymushah <tonyramanantsalama@gmail.com>
2026-01-12 16:01:36 +0300

    some connection test

--- src/test/java/lalana/LalanaTest.java
+++ src/test/java/lalana/LalanaTest.java
@@ -1,5 +1,31 @@
 package lalana;
 
+import org.junit.Test;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import lalana.data.connexion.Oracle11Connect;
+import lalana.data.connexion.PostgresConnect;
+import mg.tonymushah.dbconnection.DBConnect;
+
 public class LalanaTest {
-    
+
+	@Test
+	public void testOracleConnect() {
+		try (DBConnect connect = new Oracle11Connect(Dotenv.load())) {
+			connect.connect();
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void testPsqlConnect() {
+		try (DBConnect connect = new PostgresConnect(Dotenv.load())) {
+			connect.connect();
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+	}
 }
