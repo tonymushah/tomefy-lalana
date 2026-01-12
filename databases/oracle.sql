d8c7d8c602ae7c68fb18a78d9192b542a9c651f1
tonymushah <tonyramanantsalama@gmail.com>
2026-01-12 17:36:35 +0300

    Some database changes

--- databases/oracle.sql
+++ databases/oracle.sql
@@ -1,32 +1,97 @@
 -- L'information sur la route
-
-create table tablepoint(
-    id int,
-    x number,
-    y number
-);
-create table lalana(
-    id int ,
-    nom text ,
-    pointArrivee foreign key(tablepoint id),
-    pointDepart foreign key(tablepoint id),
-    distance number,
-    largeur number,
+CREATE TABLE tablepoint (
+	id NUMBER PRIMARY KEY,
+	x NUMBER,
+	y NUMBER
 );
 
-create table sous_lalana_correspondance(
-    id int ,
-    idLalanaMere foreign key(lalana id),
-    idLalanaFille foreign key(lalana id),
-    idOrdApparition int 
+CREATE TABLE lalana (
+	id NUMBER PRIMARY KEY,
+	nom VARCHAR2(255) not null,
+	pointArrivee NUMBER not null,
+	pointDepart NUMBER not null,
+	distance NUMBER not null,
+	largeur NUMBER not null,
+	CONSTRAINT fk_lalana_pointArrivee FOREIGN KEY (pointArrivee) REFERENCES tablepoint(id),
+	CONSTRAINT fk_lalana_pointDepart FOREIGN KEY (pointDepart) REFERENCES tablepoint(id)
+);
+
+CREATE TABLE sous_lalana_correspondance (
+	id NUMBER PRIMARY KEY,
+	idLalanaMere NUMBER not null,
+	idLalanaFille NUMBER not null,
+	idOrdApparition NUMBER not null,
+	CONSTRAINT fk_sous_lalana_mere FOREIGN KEY (idLalanaMere) REFERENCES lalana(id),
+	CONSTRAINT fk_sous_lalana_fille FOREIGN KEY (idLalanaFille) REFERENCES lalana(id)
 );
 
 -- left join 
-create table obstacle(
-    id int,
-    idLalanaMere foreign key(lalana id),
-    pourcentageReduction number ,
-    distanceArrivee number ,
-    distanceDepart number ,
+CREATE TABLE obstacle (
+	id NUMBER PRIMARY KEY,
+	idLalanaMere NUMBER not null,
+	pourcentageReduction NUMBER not null,
+	distanceArrivee NUMBER not null,
+	distanceDepart NUMBER not null,
+	CONSTRAINT fk_obstacle_lalana FOREIGN KEY (idLalanaMere) REFERENCES lalana(id)
 );
 
+CREATE SEQUENCE seq_tablepoint START WITH 1 INCREMENT BY 1 NOCACHE NOCYCLE;
+
+CREATE
+OR REPLACE TRIGGER trg_tablepoint_id BEFORE
+INSERT
+	ON tablepoint FOR EACH ROW
+	WHEN (NEW.id IS NULL) BEGIN
+SELECT
+	seq_tablepoint.NEXTVAL INTO :NEW.id
+FROM
+	dual;
+
+END;
+/ 
+
+CREATE SEQUENCE seq_lalana START WITH 1 INCREMENT BY 1 NOCACHE NOCYCLE;
+
+CREATE
+OR REPLACE TRIGGER trg_lalana_id BEFORE
+INSERT
+	ON lalana FOR EACH ROW
+	WHEN (NEW.id IS NULL) BEGIN
+SELECT
+	seq_lalana.NEXTVAL INTO :NEW.id
+FROM
+	dual;
+
+END;
+/ 
+
+CREATE SEQUENCE seq_sous_lalana_correspondance START WITH 1 INCREMENT BY 1 NOCACHE NOCYCLE;
+
+CREATE
+OR REPLACE TRIGGER trg_sous_lalana_correspondance_id BEFORE
+INSERT
+	ON sous_lalana_correspondance FOR EACH ROW
+	WHEN (NEW.id IS NULL) BEGIN
+SELECT
+	seq_sous_lalana_correspondance.NEXTVAL INTO :NEW.id
+FROM
+	dual;
+
+END;
+/ 
+
+CREATE SEQUENCE seq_obstacle START WITH 1 INCREMENT BY 1 NOCACHE NOCYCLE;
+
+CREATE
+OR REPLACE TRIGGER trg_obstacle_id BEFORE
+INSERT
+	ON obstacle FOR EACH ROW
+	WHEN (NEW.id IS NULL) BEGIN
+SELECT
+	seq_obstacle.NEXTVAL INTO :NEW.id
+FROM
+	dual;
+
+END;
+
+/
--- databases/postgres.sql
+++ databases/postgres.sql
@@ -1,14 +1,15 @@
 -- L'information sur la voiture :
 
 create table voiture_type(
-    id int ,
-    libele
--- 4*4 , caterelle ...
-)
+    id serial primary key,
+    libele text not null
+    -- 4*4 , caterelle ...
+);
+
 create table voiture (
-    id int,
-    idVoitureType int foreign key(voiture_type id),
-    largeur number ,
-    longueur number ,
-    vitesseMax number
+    id serial primary key,
+    idVoitureType int references voiture_type(id) not null,
+    largeur numeric not null,
+    longueur numeric not null,
+    vitesseMax numeric not null
 );
