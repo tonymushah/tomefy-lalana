4bc32a782b862a3885945c2f7485aae413075a92
tonymushah <tonyramanantsalama@gmail.com>
2026-01-12 16:33:15 +0300

    some changes

--- src/test/java/lalana/LalanaTest.java
+++ src/test/java/lalana/LalanaTest.java
@@ -6,25 +6,31 @@ import java.nio.file.Path;
 import org.junit.Test;
 
 import io.github.cdimascio.dotenv.Dotenv;
+import io.github.cdimascio.dotenv.DotenvEntry;
 import lalana.data.connexion.Oracle11Connect;
 import lalana.data.connexion.PostgresConnect;
 import mg.tonymushah.dbconnection.DBConnect;
 
 public class LalanaTest {
 
+	public static Dotenv testDotenv() {
+		return Dotenv.configure().load();
+	}
+
 	@Test
 	public void testOracleConnect() throws Exception {
-		System.out.println(Path.of(".").normalize());
-		assertNotNull("ORACLE_PORT_NOT_SET", Dotenv.load().get("ORACLE_PORT"));
-		try (DBConnect connect = new Oracle11Connect(Dotenv.load())) {
+		Dotenv testDotenv = testDotenv();
+		assertNotNull("ORACLE_PORT_NOT_SET", testDotenv.get("ORACLE_PORT"));
+		try (DBConnect connect = new Oracle11Connect(testDotenv)) {
 			connect.connect();
 		}
 	}
 
 	@Test
 	public void testPsqlConnect() throws Exception {
-		assertNotNull("POSTGRES_PORT_NOT_SET", Dotenv.load().get("POSTGRES_PORT"));
-		try (DBConnect connect = new PostgresConnect(Dotenv.load())) {
+		Dotenv testDotenv = testDotenv();
+		assertNotNull("POSTGRES_PORT_NOT_SET", testDotenv.get("POSTGRES_PORT"));
+		try (DBConnect connect = new PostgresConnect(testDotenv)) {
 			connect.connect();
 		}
 	}
